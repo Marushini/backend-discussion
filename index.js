c6240e529f45d97dbd87ab8c09f2f05b72c4b018
Marushini <marushinibabu2203@gmail.com>
2024-12-13 00:31:53 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware to enable CORS (allow frontend to make requests)
 const corsOptions = {
-    origin: 'http://localhost:3000',  // Replace with your frontend URL in production
+    origin: 'http://localhost:5173',  // Replace with your frontend URL in production
     methods: 'GET,POST,PUT,DELETE',  // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Authorization',  // Allowed headers
 };
