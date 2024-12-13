08ef8303fa92d2c1f20790544a87cb7781bdcd56
Marushini <marushinibabu2203@gmail.com>
2024-12-13 14:18:45 +0530

    done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware to enable CORS (allow frontend to make requests)
 const corsOptions = {
-    origin: 'http://localhost:5173',  // Replace with your frontend URL in production
+    origin: 'frontend-discussion-w7z1.vercel.app/',  // Replace with your frontend URL in production
     methods: 'GET,POST,PUT,DELETE',  // Allowed HTTP methods
     allowedHeaders: 'Content-Type, Authorization',  // Allowed headers
 };
